dbb70f37a716e6e5bde089a20a2fca48337eaf9a
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2019-09-16 00:24:26 +0200

    Add changes in HourPicker

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -1,5 +1,5 @@
 import BaseWidget from './BaseWidget.js';
-import { settings, select } from '../settings.js';
+import { select, settings } from '../settings.js';
 import {utils} from '../utils.js';
 
 class HourPicker extends BaseWidget {
@@ -7,41 +7,50 @@ class HourPicker extends BaseWidget {
     super(wrapper, settings.hours.open);
 
     const thisWidget = this;
+    thisWidget.dom = {};
+    thisWidget.dom.wrapper = wrapper;
+
 
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
     thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
-    thisWidget.initPlugin();
-    thisWidget.value = thisWidget.dom.input.value;
+
+    thisWidget.value = settings.hours.open;
+    thisWidget.initPlugin(thisWidget.value);
+
   }
 
   initPlugin() {
     const thisWidget = this;
 
-    rangeSlider.create(thisWidget.dom.input);
+    rangeSlider.create(thisWidget.dom.input, {
+      onSlide: function(value) {
+        thisWidget.value = value;
+      },
+    });
 
     // dla elementu thisWidget.dom.input dodaj listener eventu 'input'.
     // W handlerze tego eventu przypisz wartości widgetu (thisWidget.value) wartość tego elementu.
-    thisWidget.dom.input.addEventListener('input', function(){
-      thisWidget.value = thisWidget.dom.input.value; //powtórzone w konstruktorze
-    });
+    // thisWidget.dom.input.addEventListener('input', function(){
+    //   thisWidget.value = thisWidget.dom.input.value; //powtórzone w konstruktorze
+    // });
   }
 
-  parseValue(value) {
+  parseValue(newValue) {
     // Metoda parseValue ma przekazywać otrzymaną wartość do funkcji utils.numberToHour
     // i zwracać wartość otrzymaną z tej funkcji. Ta funkcja zamienia liczby na zapis godzinowy,
     //czyli np. 12 na '12:00', a 12.5 na '12:30'.
-    return utils.numberToHour(value);
+    return utils.numberToHour(newValue);
   }
 
   isValid(value) {
     // Metoda isValid może zawsze zwracać prawdę true.
-    return isNaN(value);
+    return true;
   }
 
   renderValue() {
     const thisWidget = this;
     // Metoda renderValue ma zamieniać zawartość elementu thisWidget.dom.output na wartość widgetu.
-    thisWidget.dom.output = thisWidget.value;
+    thisWidget.dom.output.innerHTML = thisWidget.value;
   }
 }
 
